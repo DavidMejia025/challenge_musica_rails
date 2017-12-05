030e6e2b11b89f27841c8e50a8c99f8db317977c
OEM Configuration (temporary user) <davidndy@gmail.com>
2017-12-05 14:17:31 -0500

    Challenge musica y mas musica second part integrating artist with albums

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,33 @@
+class ArtistsController < ApplicationController
+   def index
+  	@artist = Artist.all
+  end
+
+  def new
+  	@artist = Artist.new
+  end
+
+  def create
+  	@artist = Artist.new(artist_params)
+  	if @artist.save
+  		redirect_to artists_path
+  	else
+  		render "new"
+  	end
+  end
+
+  def show
+  	@artist = Artist.find(params[:id])
+  end
+
+  def destroy
+  	@artist = Artist.find(params[:id])
+  	@artist.destroy
+  	redirect_to artists_path
+  end
+  private
+
+  def artist_params
+  	params.require(:artist).permit(:name, :image_url)
+  end 
+end
